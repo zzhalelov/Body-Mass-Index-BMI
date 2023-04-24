48875c3c5cfe87f648731355904797f7d5699438
Zhastilek Zhalelov <zzhalelov@gmail.com>
2023-04-24 16:57:30 +0600

    1st commit

---
+++ src/BMI.java
@@ -0,0 +1,18 @@
+public class BMI {
+    public void calculatingBMI(int weight, int height) {
+        double bmi;
+        bmi = weight / ((height * 0.01) * (height * 0.01));
+        System.out.println(bmi);
+        if (bmi <= 16) {
+            System.out.println("У вас выраженный дефицит веса");
+        } else if (bmi > 16 && bmi < 17.9) {
+            System.out.println("У вас недостаток массы тела");
+        } else if (bmi > 18 && bmi < 24.9) {
+            System.out.println("Ваш вес в норме");
+        } else if (bmi > 25 && bmi < 29.9) {
+            System.out.println("У вас чрезмерная масса тела");
+        } else if (bmi > 30) {
+            System.out.println("У вас ожирение");
+        }
+    }
+}
